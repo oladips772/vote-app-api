c3a3e5fa99ef79a79e1482b2cc7b1a063364f5b1
oladips <oladips200@gmail.com>
2022-05-28 15:25:01 +0000

    make it better

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const { notFound, errorHandler } = require("./Middlewares/Error");
 
 connectDatabase();
 const app = express();
-app.use(notFound);
-app.use(errorHandler);
 app.use(express.json());
 app.use("/api/import", ImportData);
 app.use("/api/politicians", PoliticianRouter);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
